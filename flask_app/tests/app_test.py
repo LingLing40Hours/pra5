02bcce6ffd51654502dcc7ac2d9f65fd8369bf5b
Xiangyu <chenxy327@gmail.com>
2024-10-25 15:55:59 -0400

    write tests; aws account got suspended, so they're calling app locally

--- flask_app/tests/app_test.py
+++ flask_app/tests/app_test.py
@@ -2,7 +2,12 @@ import pytest
 from pathlib import Path
 import json
 from application import application
+import requests
+import time
+import csv
 
+
+BASE_URL = 'placeholder'
 
 @pytest.fixture
 def client():
@@ -24,3 +29,54 @@ def test_real1(client):
 def test_real2(client):
     result = client.post('/load_model', json={'text': 'Judge Rules White Girl Will Be Tried As Black Adult.'})
     assert result.json['prediction'] == 'REAL'
+
+def test_latency_fake1(client):
+    timestamps = []
+    for _ in range(100):
+        start_time = time.time()
+        #response = requests.post(BASE_URL, json={'text': text})
+        client.post('/load_model', json={'text': 'Noise and light pollution from car traffic kills roadside trees.'})
+        end_time = time.time()
+        timestamps.append(end_time - start_time)
+
+    write_to_csv('pollution killed trees', timestamps)
+
+def test_latency_fake2(client):
+    timestamps = []
+    for _ in range(100):
+        start_time = time.time()
+        #response = requests.post(BASE_URL, json={'text': text})
+        client.post('/load_model', json={'text': 'Toronto metro relies on cigarette smoke of passengers to keep passageways free of insects.'})
+        end_time = time.time()
+        timestamps.append(end_time - start_time)
+
+    write_to_csv('cigarettes as insect repellent', timestamps)
+
+def test_latency_real1(client):
+    timestamps = []
+    for _ in range(100):
+        start_time = time.time()
+        #response = requests.post(BASE_URL, json={'text': text})
+        client.post('/load_model', json={'text': 'ECE444 TAs go on strike as their work is being done by students attending the course.'})
+        end_time = time.time()
+        timestamps.append(end_time - start_time)
+
+    write_to_csv('ECE444 TA strike', timestamps)
+
+def test_latency_real2(client):
+    timestamps = []
+    for _ in range(100):
+        start_time = time.time()
+        #response = requests.post(BASE_URL, json={'text': text})
+        client.post('/load_model', json={'text': 'Judge Rules White Girl Will Be Tried As Black Adult.'})
+        end_time = time.time()
+        timestamps.append(end_time - start_time)
+
+    write_to_csv('ONN heading', timestamps)
+
+def write_to_csv(filename, timestamps):
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Latency (seconds)'])
+        for timestamp in timestamps:
+            writer.writerow([timestamp])
